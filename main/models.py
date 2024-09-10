86da910cc9714563a548643146b78871c644fba2
glorvibop <shaineglorvina@gmail.com>
2024-09-10 11:36:08 +0700

    changed models and fixed unit test

--- main/models.py
+++ main/models.py
@@ -2,13 +2,10 @@ from django.db import models
 
 class ChocolateProduct(models.Model):
     name_product = models.CharField(max_length=255)
-    price = models.FloatField
+    price = models.FloatField()
     description = models.TextField()
     type = models.CharField(max_length=255)
     cocoa_ratio = models.IntegerField()
-    app_name = models.CharField(max_length=255)
-    name =  models.CharField(max_length=255)
-    my_class =  models.CharField(max_length=255)
 
     @property
     def is_cocoa_ratio_strong(self):
--- main/tests.py
+++ main/tests.py
@@ -17,12 +17,9 @@ class mainTest(TestCase):
     def test_strong_cocoa_ratio(self):
         product = ChocolateProduct.objects.create(
             name_product = "Läderach",
-            price = "110.00",  # Note this should be a Decimal, not a string
+            price = 110.00,  # Note this should be a Decimal, not a string
             description = "Premium chocolate from Swiss with the freshest ingredients to create memorable moments of joy!",
             type = "Dark Chocolate",
             cocoa_ratio = 80,
-            app_name = "Bonagit Store",
-            name = "Shaine Glorvina Mathea",
-            my_class = "PBP B"
         )
         self.assertTrue(product.is_cocoa_ratio_strong)
