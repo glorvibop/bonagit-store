fcd22a1bf98226742f1687b0b06f71b76f4b6716
glorvibop <shaineglorvina@gmail.com>
2024-09-21 16:37:10 +0700

    first push tugas 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import uuid  # tambahkan baris ini di paling atas
+from django.contrib.auth.models import User
 
 class ChocolateProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product_name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=0, max_digits=5)
